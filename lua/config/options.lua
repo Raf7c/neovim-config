67f09f1ef01389620af63ca6125aa2f0d08198f3
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-01-23 02:49:15 +0100

    feat (config): option configuration for the editor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,36 @@
-local opts = {} 
+local opts = {}
+local opt = vim.opt
 function opts.init()
-  vim.opt.number = true -- Number in front of each line
+
+	-- Files & Encoding
+	opt.fileencoding = "utf-8"
+	opt.encoding = "utf-8"
+	opt.autowrite = true
+	opt.autoread = true 
+	opt.confirm = true
+
+	-- Mouse & Clipboard
+	opt.clipboard:append("unnamedplus") -- Systeme
+	opt.mouse = "a"
+
+	-- Display & UI
+	opt.number = true 
+	opt.cursorline = true
+	opt.wrap = false
+	opt.signcolumn = "yes"
+	opt.winbar = "" 
+
+	-- Indentation & Formatting
+	opt.smartindent = true  
+	opt.tabstop = 2
+	opt.shiftwidth = 2
+
+	-- Characters & Spacing
+	opt.list = true
+	opt.listchars:append("eol:↴")
+
+	-- Window management & Splits
+	opt.splitbelow = true 
+	opt.splitright = true
 end
 return opts
