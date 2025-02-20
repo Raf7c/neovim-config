d3de5e193b8c32e9856aa51af74ca8cc18dbbc18
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-02-21 00:03:52 +0100

    feat (plugins): install telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,9 @@
 return {
-  "nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-web-devicons",
-  "MunifTanjim/nui.nvim",
+	"nvim-lua/plenary.nvim",
+	"MunifTanjim/nui.nvim",
+	{
+		"nvim-tree/nvim-web-devicons",
+		dependencies = { "DaikyXendo/nvim-material-icon" },
+	},
+	{ "yamatsum/nvim-nonicons", config = true, enabled = false },
 }
