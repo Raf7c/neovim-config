692aee99e470a14895bbe0372b1d9621661def95
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-02-21 00:03:15 +0100

    feat (plugins): install neogen and mini pairs

--- init.lua
+++ init.lua
@@ -1,34 +1,33 @@
 -- Initializing lazy leader key conf
 vim.g.mapleader = " "
+-- End lazy initialization
+local opts = require("config.options")
+opts.init()
+local keymaps = require("config.keymaps")
+keymaps.init()
 
 --- Install lazy.nvim
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  }
+	vim.fn.system {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Configure lazy.nvim
 require("lazy").setup {
-  spec = {
-    { import = "plugins"},
-    { import = "plugins.ui"},
-  },
-  defaults = { lazy = true, version = nil },
-  install = { missing = true, colorscheme = { "catppuccin" } },
+	spec = {
+		{ import = "plugins" },
+		{import = "pde"},
+		{import = "plugins.colorscheme"},
+	},
+	defaults = { lazy = true, version = nil },
+	install = { missing = true, colorscheme = { "catppuccin", "tokyonight" } },
 }
 vim.keymap.set("n", "<leader>z", "<cmd>:Lazy<cr>", { desc = "Plugin Manager" })
-
--- End lazy initialization
-local opts = require("config.options")
-opts.init()
-
-local keymaps = require("config.keymaps")
-keymaps.init()
