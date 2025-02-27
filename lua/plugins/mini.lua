4fe1134928ceab56dfa8057e2cf7ee21b08b8f90
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-02-27 14:52:56 +0100

    feat: mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,4 +5,73 @@ return {
 		event = "VeryLazy",
 		opts = {},
 	},
+
+	{
+		"echasnovski/mini.icons",
+		lazy = true,
+		opts = {
+			file = {
+				[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+				["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+			},
+			filetype = {
+				dotenv = { glyph = "", hl = "MiniIconsYellow" },
+			},
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
+
+	{
+		"echasnovski/mini.indentscope",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"Trouble",
+					"alpha",
+					"dashboard",
+					"fzf",
+					"help",
+					"lazy",
+					"mason",
+					"neo-tree",
+					"notify",
+					"toggleterm",
+					"trouble",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+				end,
+			},
+		},
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
 }
