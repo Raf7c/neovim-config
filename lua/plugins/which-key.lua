abc449ceb5e5cde690db540b8eea3d0496d8611f
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-02-27 14:54:49 +0100

    feat: key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,14 +1,40 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	config = function()
-		require("which-key").register({
-			["<leader>d"] = { name = "+Diagnostics/Quickfix" },
-			["<leader>f"] = { name = "+File/Find" },
-			["<leader>g"] = { name = "+Git" },
-			["<leader>l"] = { name = "+Lsp" },
-			["<leader>t"] = { name = "+Tab" },
-			["<leader>w"] = { name = "+Windows" },
-		})
+	opts_extend = { "spec" },
+	opts = {
+		preset = "helix",
+		defaults = {},
+		spec = {
+			{
+				mode = { "n", "v" },
+				{ "<leader>t", group = "tabs" },
+				{ "<leader>l", group = "code" },
+				{ "<leader>d", group = "debug" },
+				{ "<leader>f", group = "file/find" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>s", group = "search" },
+				{ "<leader>w", group = "Window" },
+
+				--{ "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Keymaps (which-key)",
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		if not vim.tbl_isempty(opts.defaults) then
+			LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+			wk.register(opts.defaults)
+		end
 	end,
 }
