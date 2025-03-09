a705bc171f905ebf64e9ba1d5f912862aae1068e
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-09 17:53:41 +0100

    feat: sup tokyonight

--- lua/plugins/colorscheme/colorscheme.lua
+++ lua/plugins/colorscheme/colorscheme.lua
@@ -1,49 +1,14 @@
 return {
-	-- Styler for applying specific themes to specific filetypes
-	{
-		"folke/styler.nvim",
-		event = "VeryLazy",
-		opts = {
-			themes = {
-				markdown = { colorscheme = "tokyonight" },
-				help = { colorscheme = "tokyonight" },
-			},
-		},
-	},
-
-	-- Main theme: Catppuccin
+	-- Catppuccin theme with dark/light toggle
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		lazy = false,
 		priority = 1000, -- Ensure it loads before other plugins
 		opts = {
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
+			flavour = "mocha", -- Default to dark theme
 			transparent_background = true,
 			term_colors = true,
-			dim_inactive = {
-				enabled = false,
-				shade = "dark",
-				percentage = 0.15,
-			},
-			styles = {
-				comments = { "italic" },
-				conditionals = { "italic" },
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-			},
 			integrations = {
 				cmp = true,
 				nvimtree = true,
@@ -83,50 +48,37 @@ return {
 		},
 		config = function(_, opts)
 			local catppuccin = require("catppuccin")
-			catppuccin.setup(opts)
-			vim.cmd.colorscheme "catppuccin"
 			
-			-- Function to toggle between light and dark themes
-			_G.toggle_theme = function()
+			-- Initial setup with provided options
+			catppuccin.setup(opts)
+			vim.cmd("colorscheme catppuccin")
+			vim.o.background = "dark"
+			
+			-- Function to toggle between dark (mocha) and light (latte) flavors
+			_G.toggle_catppuccin = function()
 				local current_flavor = vim.g.catppuccin_flavour or "mocha"
-				local new_flavor = current_flavor == "latte" and "mocha" or "latte"
+				local new_flavor = current_flavor == "mocha" and "latte" or "mocha"
+				local mode = new_flavor == "latte" and "light" or "dark"
 				
+				-- Update configuration with new flavor
 				vim.g.catppuccin_flavour = new_flavor
 				catppuccin.setup({ flavour = new_flavor })
-				vim.cmd.colorscheme "catppuccin"
 				
-				-- Adjust background and notify user
-				vim.o.background = new_flavor == "latte" and "light" or "dark"
-				vim.notify("Switched to " .. (new_flavor == "latte" and "light" or "dark") .. " theme", vim.log.levels.INFO)
+				-- Apply changes
+				vim.cmd("colorscheme catppuccin")
+				vim.cmd("set background=" .. mode)
+				
+				-- Force refresh
+				vim.defer_fn(function()
+					vim.cmd("colorscheme catppuccin")
+					vim.cmd("redraw")
+					vim.notify("Switched to Catppuccin " .. new_flavor .. " (" .. mode .. " theme)", vim.log.levels.INFO)
+				end, 10)
 			end
 			
 			-- Create command and keybinding for theme toggling
-			vim.api.nvim_create_user_command("ToggleTheme", function() _G.toggle_theme() end, {})
-			vim.keymap.set("n", "<leader>ut", _G.toggle_theme, { desc = "Toggle between light and dark themes" })
+			vim.api.nvim_create_user_command("ToggleTheme", function() _G.toggle_catppuccin() end, {})
+			vim.keymap.set("n", "<leader>ut", _G.toggle_catppuccin, { desc = "Toggle between dark and light themes" })
 		end,
-	},
-	
-	-- Secondary theme: Tokyo Night
-	{
-		"folke/tokyonight.nvim",
-		lazy = true, -- Load on demand
-		opts = {
-			style = "moon", -- moon, storm, night, day
-			transparent = true,
-			terminal_colors = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				sidebars = "dark",
-				floats = "dark",
-			},
-			sidebars = { "qf", "help", "terminal", "packer" },
-			day_brightness = 0.3,
-			hide_inactive_statusline = false,
-			dim_inactive = false,
-			lualine_bold = false,
-		},
 	},
 }
