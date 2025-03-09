d27ebe6dd36e0802221724c22516b58bb60cf891
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-09 18:32:57 +0100

    style(coding): move mini

---
+++ lua/plugins/coding/mini-surround.lua
@@ -0,0 +1,25 @@
+return {
+  "echasnovski/mini.surround",
+  version = "*",
+  event = "VeryLazy",
+  keys = {
+    { "gsa", desc = "Add Surrounding", mode = { "n", "v" } },
+    { "gsd", desc = "Delete Surrounding" },
+    { "gsf", desc = "Find Right Surrounding" },
+    { "gsF", desc = "Find Left Surrounding" },
+    { "gsh", desc = "Highlight Surrounding" },
+    { "gsr", desc = "Replace Surrounding" },
+    { "gsn", desc = "Update `MiniSurround.config.n_lines`" },
+  },
+  opts = {
+    mappings = {
+      add = "gsa",        -- Add surrounding in Normal and Visual modes
+      delete = "gsd",     -- Delete surrounding
+      find = "gsf",       -- Find surrounding (to the right)
+      find_left = "gsF",  -- Find surrounding (to the left)
+      highlight = "gsh",  -- Highlight surrounding
+      replace = "gsr",    -- Replace surrounding
+      update_n_lines = "gsn", -- Update `n_lines`
+    },
+  },
+}
