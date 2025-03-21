30f61a6e7e5a283e5fcf49291b7fde73a6b3f260
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-21 15:04:04 +0100

    feat (ui) buffer

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -6,77 +6,29 @@ return {
   },
   event = "VeryLazy",
   keys = {
-    { "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin" },
-    { "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete non-pinned buffers" },
-    { "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete other buffers" },
-    { "<leader>br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete buffers to the right" },
-    { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete buffers to the left" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "<leader><tab>p", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<leader><tab>x", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
   },
   opts = {
     options = {
-      mode = "buffers",
+      mode = "tabs",
       diagnostics = "nvim_lsp",
-      diagnostics_indicator = function(_, _, diag)
-        local icons = require("config.icons").diagnostics
-        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warning .. diag.warning or "")
-        return vim.trim(ret)
-      end,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "File Explorer",
-          text_align = "center",
-          separator = true,
-        },
-        {
-          filetype = "NvimTree",
-          text = "File Explorer",
-          text_align = "center",
-          separator = true,
-        },
-      },
       indicator = {
-        icon = "▎", -- this should look like a vertical bar
+        icon = "▎",
         style = "icon",
       },
-      buffer_close_icon = "",
       modified_icon = "●",
-      close_icon = "",
-      left_trunc_marker = "",
-      right_trunc_marker = "",
       max_name_length = 30,
-      max_prefix_length = 30,
       tab_size = 21,
       separator_style = "thin",
-      show_buffer_icons = true,
-      show_buffer_close_icons = true,
-      show_close_icon = true,
-      show_tab_indicators = true,
+      show_buffer_icons = false,
+      show_tab_indicators = false,
       always_show_bufferline = true,
-      enforce_regular_tabs = false,
-      color_icons = true,
-      highlights = {
-        buffer_selected = {
-          italic = false,
-        },
-      },
+      enforce_regular_tabs = true,
+      color_icons = false,
     },
   },
   config = function(_, opts)
     require("bufferline").setup(opts)
-    
-    -- Set auto-commands for bufferline
-    vim.api.nvim_create_autocmd("BufAdd", {
-      callback = function()
-        vim.schedule(function()
-          pcall(nvim_bufferline)
-        end)
-      end,
-    })
   end,
 } 
