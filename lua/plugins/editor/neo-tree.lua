9b4b99c76a865472b92fd99a195b8a85e5edea9e
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-21 14:34:27 +0100

    customization icons

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -8,15 +8,51 @@ return {
   },
   keys = {
     { "<leader>e", "<cmd>Neotree toggle reveal position=float<cr>", desc = "Toggle Neotree" },
+    { "<leader>t", "<cmd>Neotree toggle reveal position=left<cr>", desc = "Toggle Neotree (left)" },
   },
   opts = {
     enable_git_status = true,
     enable_diagnostics = true,
+    window = {
+      position = "right",
+      width = 30,
+    },
     filesystem = {
       follow_current_file = {
         enabled = true,
         leave_dirs_open = false,
       },
     },
+    git_status = {
+      symbols = {
+        added = "✚",
+        modified = "●",
+        deleted = "✖",
+        renamed = "󰁕",
+        conflict = "",
+        untracked = "★",
+        ignored = "◌",
+        unstaged = "󰄱",
+        staged = "✓",
+        typechange = "󰉿",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true,
+        expander_collapsed = "󰅀",
+        expander_expanded = "󰅁",
+      },
+      icon = {
+        folder_closed = "󰉋",
+        folder_open = "󰝰",
+        folder_empty = "󰉖",
+        folder_empty_open = "󰉖",
+        default = "󰈚",
+      },
+    },
   },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+  end,
 }
