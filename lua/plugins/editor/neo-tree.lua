dde0088fb7f829473cf3591acdc9c2c47c13bc95
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-23 18:51:13 +0100

    refactor(editor): using icons with the icons.lua file

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -1,59 +1,57 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
 	keys = {
 		{ "<leader>e", "<cmd>Neotree toggle reveal position=float<cr>", desc = "Toggle Neotree" },
 		{ "<leader>t", "<cmd>Neotree toggle reveal position=left<cr>", desc = "Toggle Neotree (left)" },
 	},
-	opts = {
-		popup_border_style = "rounded",
-		enable_git_status = true,
-		enable_diagnostics = true,
-		open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-		sort_function = nil,
-		filesystem = {
-			bind_to_cwd = false,
-			follow_current_file = { enabled = true },
-			use_libuv_file_watcher = true,
-		},
-		window = {
-			position = "left",
-			width = 35,
-		},
+	opts = function()
+		local icons = require("config.icons")
 
-		git_status = {
-			symbols = {
-				added = "✚",
-				modified = "●",
-				deleted = "✖",
-				renamed = "󰁕",
-				conflict = "",
-				untracked = "★",
-				ignored = "◌",
-				unstaged = "󰄱",
-				staged = "✓",
-				typechange = "󰉿",
+		return {
+			popup_border_style = "rounded",
+			enable_git_status = true,
+			enable_diagnostics = true,
+			open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+			sort_function = nil,
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
 			},
-		},
-		default_component_configs = {
-			indent = {
-				with_expanders = true,
-				expander_collapsed = "󰅀",
-				expander_expanded = "󰅁",
+			window = {
+				position = "left",
+				width = 35,
 			},
-			icon = {
-				folder_closed = "󰉋",
-				folder_open = "󰝰",
-				folder_empty = "󰜌",
-				default = "󰈚",
-				highlight = "NeoTreeFileIcon",
+			git_status = {
+				symbols = {
+					added = "",
+					modified = "",
+					deleted = "✖",
+					renamed = icons.git.FileRenamed,
+					conflict = icons.git.FileUnmerged,
+					untracked = icons.git.FileUntracked,
+					ignored = icons.git.FileIgnored,
+					unstaged = icons.git.FileUnstaged,
+					staged = icons.git.FileStaged,
+					typechange = icons.ui.Code,
+				},
 			},
-		},
-	},
+			default_component_configs = {
+				indent = {
+					with_expanders = true,
+					expander_collapsed = icons.ui.ChevronRight,
+					expander_expanded = icons.ui.ChevronShortDown,
+				},
+				icon = {
+					folder_closed = icons.ui.Folder,
+					folder_open = icons.ui.FolderOpen,
+					folder_empty = icons.ui.EmptyFolder,
+					default = icons.ui.File,
+					highlight = "NeoTreeFileIcon",
+				},
+			},
+		}
+	end,
 	config = function(_, opts)
 		require("neo-tree").setup(opts)
 	end,
