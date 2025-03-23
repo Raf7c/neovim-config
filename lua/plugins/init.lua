4c1dd334ddf17850cdc396dc21c94036e2b07604
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-03-23 02:35:31 +0100

    refactor(ui): move in init plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,4 +6,28 @@ return {
 		dependencies = { "DaikyXendo/nvim-material-icon" },
 	},
 	{ "yamatsum/nvim-nonicons", config = true, enabled = false },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+		  indent = {
+			char = "│",
+			tab_char = "│",
+		  },
+		  scope = { enabled = false },
+		  exclude = {
+			filetypes = {
+			    "dashboard",
+				"help",
+				"lazy",
+				"mason",
+				"neo-tree",
+				"notify",
+				"toggleterm",
+				"trouble",
+			},
+		  },
+		},
+		main = "ibl",
+	  },
 }
